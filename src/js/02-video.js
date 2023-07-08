17df34c7cb6d39f3b6520d58792175898f3995b5
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-07-08 12:52:15 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,21 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const playerElement = document.getElementById('vimeo-player');
+const videoPlayer = new Player(playerElement);
+
+const saveTime = localStorage.getItem('videoplayer-current-time');
+
+if (saveTime) {
+    videoPlayer.setCurrentTime(saveTime);
+  }
+
+  function handleTimeUpdate(evt) {
+    const saveCurrentTime = evt.seconds;
+    console.log(saveCurrentTime);
+    localStorage.setItem('videoplayer-current-time', saveCurrentTime);
+
+  }
+  
+  videoPlayer.on('timeupdate', throttle(handleTimeUpdate, 1000));
 
